ced22e0d154638ed35d1596c3f137ddac75ce63f
Kumar1531D <164643227+Kumar1531D@users.noreply.github.com>
2024-09-22 10:14:55 +0530

    Day 4

---
+++ SwapAdjacentinLRString_777.java
@@ -0,0 +1,32 @@
+public boolean canTransform(String start, String end) {
+        if(!start.replace("X","").equals(end.replace("X","")))
+            return false;
+
+        int i = 0;
+        int j = 0;
+
+        while(i<start.length() && j<end.length()){
+            while(i<start.length() && start.charAt(i)=='X')
+                i++;
+            while(j<end.length() && end.charAt(j)=='X')
+                j++;
+
+            if(i==start.length() && j==end.length())
+                return true;
+            
+            if(i==start.length() || j==end.length())
+                return false;
+
+            if(start.charAt(i)=='R' && i>j)
+                return false;
+
+            if(start.charAt(i)=='L' && i<j)
+                return false;
+
+            i++;
+            j++;
+            
+        }
+
+        return true;
+    }
